58235b2ae5329a13dcc834de6d88e75bd45cb643
vicky435435 <54996805+vicky435435@users.noreply.github.com>
2021-01-06 19:59:56 +0530

    Update datatype.c

--- datatype.c
+++ datatype.c
@@ -117,3 +117,263 @@ int main() {
 
 
 
+//=============================================         Integer DATA Type Program         ================================================================
+
+
+
+
+
+//   1 Program 
+
+#include <stdio.h>
+void main() {
+    //int age;   
+    //age = 200;
+
+    int age = 20;
+    printf("%d \n",age);
+ }
+
+
+// 2 simple one line program 
+
+#include <stdio.h>
+int main()
+{
+    int testInteger = 5;
+    printf("Number = %d", testInteger);
+    return 0;
+}
+
+
+
+
+//   3. scanf function program 
+
+
+#include <stdio.h>
+int main()
+{
+int test1;
+printf("Enter an integer: ");
+scanf("%d", &test1);
+printf("Number = %d",test1);
+ return 0;
+}
+
+
+//  4 .   scanf function usting stroring data program 
+
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    
+    return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+    int testInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &testInteger);  
+    printf("Number = %d",testInteger);
+    return 0;
+}
+
+// /*====================================================================================================================== */
+
+
+
+
+//             using scanf function addtion program -->
+
+
+
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 - number2;      
+    
+    printf("%d - %d = %d", number1, number2, sum);
+    return 0;
+}
+
+
+// ========================================================================================================
+
+#include<stdio.h>    
+int main(){    
+int x=0,y=0,result=0;  
+  
+printf("enter first number:");  
+scanf("%d",&x);  
+printf("enter second number:");  
+scanf("%d",&y);  
+  
+result=x+y;  
+printf("sum of 2 numbers:%d ",result);  
+  
+return 0;  
+}
+
+
+// //========================================================================================================
+
+
+//  int Variable Declaration and Variable Initialization in addition program :-
+
+#include <stdio.h>
+  void main() {
+    int number1 = 22;
+    int number2 = 23;
+    int c;
+    c = number1 + number2 ;
+    printf("Sum of above two Number : %d \n",c);
+ }  
+
+
+
+// Program to cube root  integer numbers using scanf function program
+
+#include<stdio.h>    
+int main(){    
+int number;    
+printf("enter a number:");    
+scanf("%d",&number);    
+printf("cube of number is:-  ''   %d ",number*number*number);    
+return 0;  
+}
+
+
+// ----  suqre root program 
+
+#include<stdio.h>    
+int main(){    
+int number;    
+printf("enter a number:");    
+scanf("%d",&number);    
+printf("cube of number is:-  ''   %d ",number*number);    
+return 0;  
+}
+
+
+// //=============================================================================================================================================
+
+
+
+//  C program for the above approach
+
+
+#include <math.h>
+#include <stdio.h>
+ 
+// Function to find the square-root of N
+double findSQRT(double N) { return sqrt(N); }  
+ 
+// Driver Code
+int main()
+{
+ 
+    // Given number
+    int N = 12;
+ 
+    // Function call
+    printf("%f ", findSQRT(N));
+    return 0;
+}
+
+
+
+
+
+
+// /*
+//  * C program to find square root of a number   
+//  * Example: C sqrt() Function
+//  */
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double num, root;
+
+    /* Input a number from user */
+    printf("Enter any number to find square root: ");
+    scanf("%lf", &num);
+
+    /* Calculate square root of num */
+    root = sqrt(num);
+
+    /* Print the resultant value */
+    printf("Square root of %.2lf = %.2lf", num, root);
+
+    return 0;
+}
+
+
+//  Program to find square root of any number
+
+
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+   double number, squareRoot;
+
+   printf("Enter a number: ");
+   scanf("%lf", &number);
+
+   // computing the square root
+   squareRoot = sqrt(number);
+
+   printf("Square root of %.2lf =  %.2lf", number, squareRoot);
+
+   return 0;
+}
+
+
+
+// /* User defined function sum that has two int
+//  * parameters. The function adds these numbers and
+//  * return the result of addition.
+//  *   */
+
+
+#include <stdio.h>                                                       
+int sum(int a, int b){
+   return a+b;
+}
+int main()
+{
+   int num1, num2, num3;
+   printf("Enter first number: ");
+   scanf("%d", &num1);
+   printf("Enter second number: ");
+   scanf("%d", &num2);
+
+   //Calling the function
+   num3 = sum(num1, num2);
+   printf("Sum of the entered numbers: %d", num3);
+   return 0;
+}
+
+
