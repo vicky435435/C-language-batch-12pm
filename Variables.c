f12da902e5fb2e2ee946951b4a2be45fd49dbac9
vicky435435 <54996805+vicky435435@users.noreply.github.com>
2021-01-19 20:15:09 +0530

    Update Variables.c

--- Variables.c
+++ Variables.c
@@ -164,8 +164,67 @@ void main()
     printf("b = %d \n",b);
 }
 
+// 9	You can access a variable only within same or its inner block. For example -
+
+#include <stdio.h>
+
+ int main()
+	{
+	    int outervariable = 10;
+		    {
+	        printf("I can access outervariable in inner block = %d\n", outervariable);
+	    }
+	
+	    printf("I can also access outervariable in same block = %d", outervariable);
+
+    return 0;
 
 
+//10.	Inner block shadows outer block variable if both declared with same name. If both inner and outer block variable is declared with same name, then you cannot access outer block variable with same name inside inner block. For example -
+
+	#include 
+	
+	int main()
+	{
+    int num = 10; 
+
+        // Accesses outer block num
+	    printf("'num' is accessible within same block = %d\n", num);
+	
+     {
+	        // Declare num again but in different block.
+	        int num = 20;
+
+	        // Access inner block num
+         printf("Inner block 'num' shadows outer block 'num' = %d\n", num);
+	    }
+	
+	    return 0;
+}
 
 
+//  11.	Variables declared within inner block are not accessible to outer block. For example -
+
+
+	#include 
+	
+	int main()
+	{
+    int num1 = 10;
+    int num2 = 20; 
+	
+	    printf("'num1=%d' and 'num2=%d' declared in outer block is accessible within same block.\n", num1, num2);
+	
+	    {
+	        // num1 and num2 are accessible within inner block
+	        int sum = num1 + num2;
+	
+	        printf("sum = %d\n", sum);
+	    }
+	
+	    // sum is declared inside inner block hence is not accessible to outer block
+	    // printf("sum = %d\n", sum);
+	
+	    return 0;
+}
 
